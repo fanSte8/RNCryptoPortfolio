8ec31c0199227a32d7c81c652896c95bb2def324
Stefan <stefanmitov8@gmail.com>
2021-11-04 12:00:51 +0200

    add keyboard avoiding view for form

--- src/components/TradingForm.js
+++ src/components/TradingForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, KeyboardAvoidingView, ScrollView } from 'react-native';
 import tw from 'tailwind-rn';
 import data from '../data/data';
 import Colors from '../constants/color';
@@ -9,8 +9,6 @@ const TradingForm = ({ coinPrice }) => {
   const [price, setPrice] = useState(0);
   const [fee, setFee] = useState(0);
   const [amountOption, setAmountOption] = useState('');
-
-  console.log(amountOption);
 
   const handleAmountChange = amount => {
     setAmountOption('');
@@ -27,52 +25,54 @@ const TradingForm = ({ coinPrice }) => {
   };
 
   return (
-    <View style={tw('p-2')}>
-      <View style={tw('mt-4')}>
-        <Text style={tw('text-gray-400')}>Estimated purchase value</Text>
-        <TextInput 
-          keyboardType="decimal-pad"
-          value={amount.toString()} 
-          onChangeText={handleAmountChange}
-          style={tw('border-b border-gray-400 text-xl')} 
-        />
-      </View>
-      <View style={tw('mt-4 flex-row items-center')}>
-        <View style={tw('flex-1')}>
-          <Text style={tw('text-gray-400')}>Trade value</Text>
+    <ScrollView>
+      <KeyboardAvoidingView behavior="height" style={tw('p-2')}>
+        <View style={tw('mt-4')}>
+          <Text style={tw('text-gray-400')}>Estimated purchase value</Text>
           <TextInput 
             keyboardType="decimal-pad"
-            value={price.toString()} 
-            onChangeText={handlePriceChange}
+            value={amount.toString()} 
+            onChangeText={handleAmountChange}
             style={tw('border-b border-gray-400 text-xl')} 
           />
         </View>
-        <View style={tw('flex-row')}>
-          <TouchableOpacity 
-            style={{ ...tw('border p-1 m-1'), borderColor: amountOption === '25%' ? Colors.iconColor : '#ccc'}}
-            onPress={() => handlePriceChange(data.balance / 4, '25%')}
-          >
-            <Text style={{ color: amountOption === '25%' ? Colors.iconColor : '#ccc' }}>25%</Text>
-          </TouchableOpacity>
-          <TouchableOpacity 
-            style={{ ...tw('border p-1 m-1'), borderColor: amountOption === '50%' ? Colors.iconColor : '#ccc'}}
-            onPress={() => handlePriceChange(data.balance / 2, '50%')}
-          >
-            <Text style={{ color: amountOption === '50%' ? Colors.iconColor : '#ccc' }}>50%</Text>
-          </TouchableOpacity>
-          <TouchableOpacity 
-            style={{ ...tw('border p-1 m-1'), borderColor: amountOption === '100%' ? Colors.iconColor : '#ccc'}}
-            onPress={() => handlePriceChange(data.balance, '100%')}
-          >
-            <Text style={{ color: amountOption === '100%' ? Colors.iconColor : '#ccc' }}>100%</Text>
-          </TouchableOpacity>
+        <View style={tw('mt-4 flex-row items-center')}>
+          <View style={tw('flex-1')}>
+            <Text style={tw('text-gray-400')}>Trade value</Text>
+            <TextInput 
+              keyboardType="decimal-pad"
+              value={price.toString()} 
+              onChangeText={handlePriceChange}
+              style={tw('border-b border-gray-400 text-xl')} 
+            />
+          </View>
+          <View style={tw('flex-row')}>
+            <TouchableOpacity 
+              style={{ ...tw('border p-1 m-1'), borderColor: amountOption === '25%' ? Colors.iconColor : '#ccc'}}
+              onPress={() => handlePriceChange(data.balance / 4, '25%')}
+            >
+              <Text style={{ color: amountOption === '25%' ? Colors.iconColor : '#ccc' }}>25%</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              style={{ ...tw('border p-1 m-1'), borderColor: amountOption === '50%' ? Colors.iconColor : '#ccc'}}
+              onPress={() => handlePriceChange(data.balance / 2, '50%')}
+            >
+              <Text style={{ color: amountOption === '50%' ? Colors.iconColor : '#ccc' }}>50%</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              style={{ ...tw('border p-1 m-1'), borderColor: amountOption === '100%' ? Colors.iconColor : '#ccc'}}
+              onPress={() => handlePriceChange(data.balance, '100%')}
+            >
+              <Text style={{ color: amountOption === '100%' ? Colors.iconColor : '#ccc' }}>100%</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-      <View style={tw('mt-4')}>
-        <Text style={tw('text-gray-400')}>Trade fee</Text>
-        <Text style={tw('text-gray-400 text-xl')}>{`0.20% = ${fee} €`}</Text>
-      </View>
-    </View>
+        <View style={tw('mt-4')}>
+          <Text style={tw('text-gray-400')}>Trade fee</Text>
+          <Text style={tw('text-gray-400 text-xl')}>{`0.20% = ${fee} €`}</Text>
+        </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
 
