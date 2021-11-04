a5a0d8ec5520a753b239d8e5bd21e099ed50f831
Stefan <stefanmitov8@gmail.com>
2021-11-04 10:21:35 +0200

    add navigation when a currency is selected

--- src/components/CryptoCurrencyItem.js
+++ src/components/CryptoCurrencyItem.js
@@ -1,23 +1,29 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react'
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import tw from 'tailwind-rn';
 
 const CryptoCurrencyItem = ({ name, symbol, price, amount, imageSource }) => {
+  const navigation = useNavigation();
   return (
-    <View style={tw('flex-row items-center pt-4 pb-4 border-b border-gray-300')}>
-      <Image style={tw('mr-2 w-10 h-10')} source={imageSource} />
-      <View style={tw('flex-1 flex-row justify-between')}>
-        <View>
-          <Text style={tw('font-bold text-lg')}>{symbol.toUpperCase()}</Text>
-          <Text style={tw('font-bold text-gray-400')}>{name}</Text>
-        </View>
-        <View>
-          <Text style={tw('text-right font-bold text-lg')}>{`€ ${price.toFixed(2)}`}</Text>
-          <Text style={tw('text-right text-gray-400')}>{`${amount} ${symbol.toUpperCase()}`}</Text>    
+    <TouchableOpacity
+      onPress={() => navigation.navigate(symbol) }
+    >
+      <View style={tw('flex-row items-center pt-4 pb-4 border-b border-gray-300')}>
+        <Image style={tw('mr-2 w-10 h-10')} source={imageSource} />
+        <View style={tw('flex-1 flex-row justify-between')}>
+          <View>
+            <Text style={tw('font-bold text-lg')}>{symbol.toUpperCase()}</Text>
+            <Text style={tw('font-bold text-gray-400')}>{name}</Text>
+          </View>
+          <View>
+            <Text style={tw('text-right font-bold text-lg')}>{`€ ${price.toFixed(2)}`}</Text>
+            <Text style={tw('text-right text-gray-400')}>{`${amount} ${symbol.toUpperCase()}`}</Text>    
+          </View>
         </View>
       </View>
-    </View>
-  )
-}
+    </TouchableOpacity>
+  );
+};
 
 export default CryptoCurrencyItem;
