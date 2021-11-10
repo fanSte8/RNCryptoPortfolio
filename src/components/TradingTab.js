d81097bee4fc599d30d4a54d3e7058ea71c018fa
Stefan <stefanmitov8@gmail.com>
2021-11-10 08:53:55 +0200

    switch to twrnc

--- src/components/TradingTab.js
+++ src/components/TradingTab.js
@@ -8,7 +8,7 @@ const TradingTab = ({ imageSource, symbol, name, currentPrice }) => {
   const Tab = createMaterialTopTabNavigator();
 
   return (
-    <View style={tw`m-2 p-2 h-full`}>
+    <View style={tw`m-2 p-2 flex-1`}>
       <View style={tw`m-4 flex-row justify-between items-center justify-between`}>
         <View style={tw`flex-row items-center justify-between`}>
           <Image style={tw`h-6 w-6`}
